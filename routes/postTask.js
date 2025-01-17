25a683dabd011aedf57990be76207f17a2c5500e
S. Gaurisankar <gaurisankar2003@gmail.com>
2025-01-17 13:09:20 +0530

    Added date error handling

--- routes/postTask.js
+++ routes/postTask.js
@@ -1,8 +1,14 @@
 const Task = require('../models/taskSchema');
+const datefn = require('date-fns');
 
 const postTask = async (req, res) => {
     try {
         const { title, description, status, dueDate } = req.body;
+        if(datefn.isPast(dueDate)){
+            return res.status(400).json({
+                error: 'Due date cannot be in the past'
+            });
+        }
         const newTask = new Task({ title, description, status, dueDate });
         await newTask.save();
         res.status(201).json(newTask);
