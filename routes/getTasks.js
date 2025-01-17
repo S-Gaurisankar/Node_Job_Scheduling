76d9818cf7ba626b3920cf1a452202a7555d3e72
S. Gaurisankar <gaurisankar2003@gmail.com>
2025-01-17 13:06:20 +0530

    Added server code

--- routes/getTasks.js
+++ routes/getTasks.js
@@ -3,7 +3,7 @@ const Task = require('../models/taskSchema');
 const getTasks = async (req, res) => {
     try {
         const tasks = await Task.find();
-        res.json(tasks);
+        res.status(200).json(tasks);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
